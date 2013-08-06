ddb82771b3b21417d3fa25278e2fd4ad0687a5bf
Joe Nelson <cred+github@begriffs.com>
2013-08-05 22:37:19 -0700

    Generated with generator-omni-module

---
+++ test/existence.js
@@ -0,0 +1,9 @@
+var Jvscrpt = require('../module'),
+  chai       = require('chai'),
+  expect     = chai.expect;
+
+describe('Jvscrpt', function() {
+  it('exists', function() {
+    expect(Jvscrpt).to.be.an('object');
+  });
+});
